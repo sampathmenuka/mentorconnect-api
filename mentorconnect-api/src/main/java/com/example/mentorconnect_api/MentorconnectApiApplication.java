c71feca3aded575b87e4dd448f16588d4925a2e7
sampathmenuka <sampath.m.chandimal99@gmail.com>
2026-06-11 11:16:21 +0530

    Initial commit

---
+++ mentorconnect-api/src/main/java/com/example/mentorconnect_api/MentorconnectApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.mentorconnect_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MentorconnectApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MentorconnectApiApplication.class, args);
+	}
+
+}
---
+++ mentorconnect-api/src/test/java/com/example/mentorconnect_api/MentorconnectApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mentorconnect_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MentorconnectApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
